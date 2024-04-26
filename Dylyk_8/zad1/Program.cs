08d32577eb92ecc70fe4cfc4b4a32cc34a2e1049
zolduck2 <eprari@bk.ru>
2024-04-26 13:10:54 +0300

    Сделал задания по теме №8

---
+++ Dylyk_8/zad1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TRAIN
+{
+    public string Destination { get; set; }
+    public string TrainNumber { get; set; }
+    public string DepartureTime { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<TRAIN> trains = new List<TRAIN>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write("Введите пункт назначения: ");
+            string destination = Console.ReadLine();
+
+            Console.Write("Введите номер поезда: ");
+            string trainNumber = Console.ReadLine();
+
+            Console.Write("Введите время отправления: ");
+            string departureTime = Console.ReadLine();
+
+            trains.Add(new TRAIN { Destination = destination, TrainNumber = trainNumber, DepartureTime = departureTime });
+        }
+
+        trains.Sort((a, b) => string.Compare(a.DepartureTime, b.DepartureTime));
+
+        Console.Write("Введите пункт назначения для поиска: ");
+        string searchDestination = Console.ReadLine();
+
+        bool found = false;
+        foreach (var train in trains)
+        {
+            if (train.Destination == searchDestination)
+            {
+                Console.WriteLine($"Поезд {train.TrainNumber} отправляется в {train.Destination} в {train.DepartureTime}");
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine($"Поездов, направляющихся в {searchDestination}, не найдено.");
+        }
+    }
+}
